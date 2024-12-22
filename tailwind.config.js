c7aa0fd7035c9e1bb2df7e89baf21753c2ee022b
saken-k <skar.ksk@gmail.com>
2024-12-22 14:25:27 +0200

    main changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        eukraine: ['e-Ukraine Head', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
